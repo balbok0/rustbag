c78207f8cbf6a4c513c457eb2c05af6b7217a6e9
Jakub Filipek <jfilipek@proton.me>
2024-02-27 13:38:46 -0800

    finish "Meta" for bag parsing

--- crates/rosbags_lib/src/meta.rs
+++ crates/rosbags_lib/src/meta.rs
@@ -8,26 +8,24 @@ use crate::{iterators::RecordBytesIterator, records::{record::Record, connection
 #[derive(Debug, Clone)]
 pub(crate) struct Meta {
     pub(crate) topic_to_connections: HashMap<String, Vec<Connection>>,
+    pub(crate) chunk_infos: Vec<ChunkInfo>,
 }
 
 impl Meta {
     pub(crate) fn try_new_from_bytes(bytes: Bytes) -> Result<Self> {
         println!("Num bytes: {}", bytes.len());
         let mut topic_to_connections = HashMap::new();
+        let mut chunk_infos = Vec::new();
 
         for (record, data_bytes) in RecordBytesIterator::new(bytes) {
-            let dummy = match record {
+            match record {
                 Record::Connection(con) => {
-                    {
-                        con.data.get_or_init(|| ConnectionData::try_new(data_bytes).unwrap());
-                    }
+                    con.data.get_or_init(|| ConnectionData::try_new(data_bytes).unwrap());
                     topic_to_connections.entry(con._topic.clone()).or_insert(Vec::new()).push(con);
-                    3
                 },
                 Record::ChunkInfo(chunk_info) => {
-                    let chunk_datas = chunk_info.data.get_or_init(|| ChunkInfo::new_chunk_info_data_entries_from_bytes(&chunk_info, data_bytes).unwrap());
-                    println!("Chunk info datas lenght: {}", chunk_datas.len());
-                    4
+                    chunk_info.data.get_or_init(|| ChunkInfo::new_chunk_info_data_entries_from_bytes(&chunk_info, data_bytes).unwrap());
+                    chunk_infos.push(chunk_info);
                 },
                 _ => {
                     return Err(RosError::InvalidHeader("Got header type other than Connection or ChunkInfo at the end of file.").into());
@@ -35,8 +33,11 @@ impl Meta {
             };
         }
 
+        chunk_infos.sort_unstable_by_key(|ci| ci._start_time);
+
         Ok(Meta {
             topic_to_connections,
+            chunk_infos,
         })
     }
 }
